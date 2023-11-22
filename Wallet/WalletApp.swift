460155bece47019ce04e28b0481051c645c3eb18
ha-ny <hany01.dev@gmail.com>
2023-11-22 09:52:52 +0900

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by 김하은 on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
